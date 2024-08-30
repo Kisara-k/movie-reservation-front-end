7f52b5b843d20f12aa8aea015f19a987c077bd28
Kisara Kodithuwakku <kisarakd@gmail.com>
2024-08-30 20:24:23 +0530

    clean up movie form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,10 @@
 'use client';
-import { MovieForm } from './movieForm';
 import { MovieList } from './movieList';
-
-const refreshMovies = () => {
-  // Logic to refresh the movie list
-};
 
 export default function Home() {
   return (
     <main>
       <MovieList />
-      <MovieForm onMovieAdded={refreshMovies} />
     </main>
   );
 }
